4fcb7e944d79007f4588df49e300840b4dd0c8fa
Nachbar90 <marianklueh@gmail.com>
2015-11-29 15:30:34 +0100

    -Created IntRewardListener and StringRewardListener -Sample Project modified to fit new Listeners

--- Example/src/main/java/com/kluehspies/marian/example/MainActivity.java
+++ Example/src/main/java/com/kluehspies/marian/example/MainActivity.java
@@ -9,52 +9,15 @@ import com.kluehspies.marian.example.notifier.PushNotifier;
 import com.kluehspies.marian.example.notifier.ToastNotifier;
 import com.kluehspies.marian.example.trigger.Dialog;
 import com.kluehspies.marian.example.trigger.RewardView;
+import com.kluehspies.marian.unlockmanager.listener.IntRewardListener;
 import com.kluehspies.marian.unlockmanager.listener.RewardListener;
+import com.kluehspies.marian.unlockmanager.listener.StringRewardListener;
 import com.kluehspies.marian.unlockmanager.persistence.PersistenceHandler;
 import com.kluehspies.marian.unlockmanager.trigger.AndroidAchievementUnlocker;
 import com.kluehspies.marian.unlockmanager.trigger.SharedPreferencesHandler;
 import com.kluehspies.marian.unlockmanager.trigger.Trigger;
 
 public class MainActivity extends AppCompatActivity {
-
-    private static class IntegerRewardListener implements RewardListener<Integer>{
-
-        private final View parentView;
-
-        public IntegerRewardListener(View parentView){
-            this.parentView = parentView;
-        }
-
-        @Override
-        public void unlockSucceeded(Integer resourceID, Trigger trigger) {
-            SnackbarManager.getInstance().makeTextOnly(parentView, String.format("Unlock succeeded for: %s", resourceID));
-        }
-
-        @Override
-        public void unlockFailed(Integer resourceID, Trigger trigger) {
-
-        }
-
-    }
-    private static class StringRewardListener implements RewardListener<String>{
-
-        private final View parentView;
-
-        public StringRewardListener(View parentView){
-            this.parentView = parentView;
-        }
-
-        @Override
-        public void unlockSucceeded(String resourceID, Trigger<String> trigger) {
-            SnackbarManager.getInstance().makeTextOnly(parentView, String.format("Unlock succeeded for: %s", resourceID));
-        }
-
-        @Override
-        public void unlockFailed(String resourceID, Trigger<String> trigger) {
-
-        }
-
-    }
 
     private AndroidAchievementUnlocker unlocker;
 
@@ -65,29 +28,64 @@ public class MainActivity extends AppCompatActivity {
 
         unlocker = AndroidAchievementUnlocker.getDefault();
 
-        PersistenceHandler<Integer> persistenceHandler = new SharedPreferencesHandler<>(Integer.class,getApplicationContext(),"integer_key");
+        PersistenceHandler<Integer> persistenceHandler = new SharedPreferencesHandler<>(Integer.class, getApplicationContext(), "integer_key");
         unlocker.bindPersistenceHandler(persistenceHandler);
 
         SnackbarManager.getInstance().resume();
 
+        ///////////////////////////////////Listeners///////////////////////////////////////
+
         //The listener will be called, if resources 1, 2, 3 or 4 are touched
-        unlocker.bindListener(new IntegerRewardListener(findViewById(R.id.parent)), 1, 2, 3, 4);
-        unlocker.bindListener(new StringRewardListener(findViewById(R.id.parent)), "a", "b", "c");
+        unlocker.bindListener(
+                new IntRewardListener() {
+                    @Override
+                    public void unlockSucceeded(Integer item, Trigger<Integer> trigger) {
+                        SnackbarManager.getInstance().makeTextOnly(
+                                findViewById(R.id.parent),
+                                String.format("Unlock succeeded for: %s", item));
+                    }
 
-        RewardView integerUnlockView = new RewardView(this,new Trigger<>(Integer.class,1,2,3,4));
-        LinearLayout parent = (LinearLayout) findViewById(R.id.parent);
-        parent.addView(integerUnlockView);
+                    @Override
+                    public void unlockFailed(Integer item, Trigger<Integer> trigger) {
 
-        RewardView stringUnlockView = new RewardView(this,new Trigger<>(String.class,"a","b","c"));
-        parent.addView(stringUnlockView);
+                    }
+                }, 1, 2, 3, 4);
 
+        unlocker.bindListener(
+                new StringRewardListener() {
+                    @Override
+                    public void unlockSucceeded(String item, Trigger<String> trigger) {
+                        SnackbarManager.getInstance().makeTextOnly(
+                                findViewById(R.id.parent),
+                                String.format("Unlock succeeded for: %s", item));
+                    }
+
+                    @Override
+                    public void unlockFailed(String item, Trigger<String> trigger) {
+
+                    }
+                }, "a", "b", "c");
+
+
+        //Listeners get notified if an unlock succeeds/fails
         unlocker.bindListener(new PushNotifier(this), 4);
         unlocker.bindListener(new ToastNotifier(this), 3);
         unlocker.bindListener(new PushNotifier(this), "b");
         unlocker.bindListener(new ToastNotifier(this), "c");
 
+
+        ///////////////////////////////////Triggers///////////////////////////////////////
         //Dialog can unlock resource 1
-        new Dialog(this,new Trigger<>(Integer.class,1)).show();
+        new Dialog(this, new Trigger<>(Integer.class, 1)).show();
+
+        //RewardView gets notified if 1,2,3 or 4 are unlocked
+        RewardView integerUnlockView = new RewardView(this, new Trigger<>(Integer.class, 1, 2, 3, 4));
+        LinearLayout parent = (LinearLayout) findViewById(R.id.parent);
+        parent.addView(integerUnlockView);
+
+        //RewardView gets notified if a,b or c are unlocked
+        RewardView stringUnlockView = new RewardView(this, new Trigger<>(String.class, "a", "b", "c"));
+        parent.addView(stringUnlockView);
 
         unlocker.triggerUnlockIfAvailable(1);
     }
---
+++ app/src/main/java/com/kluehspies/marian/unlockmanager/listener/StringRewardListener.java
@@ -0,0 +1,7 @@
+package com.kluehspies.marian.unlockmanager.listener;
+
+/**
+ * Created by Marian Klühspies on 29.11.2015.
+ */
+public interface StringRewardListener extends RewardListener<String> {
+}
