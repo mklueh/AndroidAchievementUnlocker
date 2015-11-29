e99385e45a323f397ed0306c2738c2a9551ac4d5
Nachbar90 <marianklueh@gmail.com>
2015-11-29 15:32:04 +0100

    Created IntRewardListener and StringRewardListener

--- app/src/main/java/com/kluehspies/marian/unlockmanager/listener/IntRewardListener.java
+++ app/src/main/java/com/kluehspies/marian/unlockmanager/listener/IntRewardListener.java
@@ -3,5 +3,4 @@ package com.kluehspies.marian.unlockmanager.listener;
 /**
  * Created by Marian Klühspies on 29.11.2015.
  */
-public interface IntRewardListener extends RewardListener<Integer> {
-}
+public interface IntRewardListener extends RewardListener<Integer> {}
--- app/src/main/java/com/kluehspies/marian/unlockmanager/listener/StringRewardListener.java
+++ app/src/main/java/com/kluehspies/marian/unlockmanager/listener/StringRewardListener.java
@@ -3,5 +3,4 @@ package com.kluehspies.marian.unlockmanager.listener;
 /**
  * Created by Marian Klühspies on 29.11.2015.
  */
-public interface StringRewardListener extends RewardListener<String> {
-}
+public interface StringRewardListener extends RewardListener<String> {}
